4cbef5d11c1569b0b065bff01c8af4edbac186c9
udibagas <udibagas@gmail.com>
2026-05-08 21:19:25 +0700

    tugas oop

---
+++ tugas/oop.js
@@ -0,0 +1,4 @@
+// buat class dengan menerapkan konsep
+// encapsulation (property private, getter, setter)
+// inheritance (buat 2 kelas turunan)
+// polymorphism (overide contructor dan min 1 method yg lain)
